8ebbe3c4c61ecde3764e87b7ef7a783e40fdf500
Sam Monterola <sam@monterola.com>
2024-07-02 18:19:46 -0500

    finished board

---
+++ index.js
@@ -0,0 +1,9 @@
+const tiles = document.querySelectorAll(".tile");
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+let turn = PLAYER_X;
+
+const boardState = Array(tiles.length)
+boardState.fill(null)
+
+const strike = document.getElementById("strike")
