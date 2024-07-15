e8aa25f862f67e69fdfcddece314c28dab2acc90
Sam Monterola <sam@monterola.com>
2024-07-15 17:45:03 -0500

    adding movement functionality

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -31,6 +31,37 @@ for (let color = 0; color < 2; color++) {
         }
     }
 }
+let activePiece: HTMLElement | null = null;
+
+function grabPiece(e: React.MouseEvent) {
+    const element = e.target as HTMLElement;
+    if (element.classList.contains("chess-piece")) {
+        console.log(e);
+        const x = e.clientX - 50; //fix this when flex scaling
+        const y = e.clientY - 50;
+        element.style.position = "absolute";
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+
+        activePiece = element;
+    }
+}
+
+function movePiece(e: React.MouseEvent) {
+    if (activePiece) {
+        const x = e.clientX - 50; //fix this when flex scaling
+        const y = e.clientY - 50;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`;
+        activePiece.style.top = `${y}px`;
+    }
+}
+
+function dropPiece(e: React.MouseEvent) {
+    if (activePiece) {
+        activePiece = null;
+    }
+}
 
 export default function Chessboard() {
     let board = [];
@@ -44,8 +75,17 @@ export default function Chessboard() {
                     image = p.image;
                 }
             })
-            board.push(<Tile image={image} number={number}/>)
+            board.push(<Tile key={`${i}${j}`} image={image} number={number}/>)
         }
     }
-    return <div id="chessboard">{board}</div>;
+    return (
+        <div 
+            onMouseMove={(e) => movePiece(e)}
+            onMouseDown={(e) => grabPiece(e)} 
+            onMouseUp  ={(e) => dropPiece(e)}  
+            id="chessboard"
+        >   
+            {board}
+        </div>
+    );
 }
