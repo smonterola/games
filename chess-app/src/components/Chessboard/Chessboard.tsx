a785091b90e1ce7902fbb7c161b48d5e47694091
Sam Monterola <sam@monterola.com>
2024-07-15 20:15:15 -0500

    adding boundaries

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, useRef } from "react";
 import Tile from "../Tile/Tile";
 import "./Chessboard.css";
 
@@ -31,39 +32,59 @@ for (let color = 0; color < 2; color++) {
         }
     }
 }
-let activePiece: HTMLElement | null = null;
-
-function grabPiece(e: React.MouseEvent) {
-    const element = e.target as HTMLElement;
-    if (element.classList.contains("chess-piece")) {
-        console.log(e);
-        const x = e.clientX - 50; //fix this when flex scaling
-        const y = e.clientY - 50;
-        element.style.position = "absolute";
-        element.style.left = `${x}px`;
-        element.style.top = `${y}px`;
-
-        activePiece = element;
-    }
-}
-
-function movePiece(e: React.MouseEvent) {
-    if (activePiece) {
-        const x = e.clientX - 50; //fix this when flex scaling
-        const y = e.clientY - 50;
-        activePiece.style.position = "absolute";
-        activePiece.style.left = `${x}px`;
-        activePiece.style.top = `${y}px`;
-    }
-}
-
-function dropPiece(e: React.MouseEvent) {
-    if (activePiece) {
-        activePiece = null;
-    }
-}
 
 export default function Chessboard() {
+    const chessboardRef = useRef<HTMLDivElement>(null);
+    
+    let activePiece: HTMLElement | null = null;
+    
+    function grabPiece(e: React.MouseEvent) {
+        const element = e.target as HTMLElement;
+        if (element.classList.contains("chess-piece")) {
+            const x = e.clientX - 50; //fix this when flex scaling
+            const y = e.clientY - 50;
+            element.style.position = "absolute";
+            element.style.left = `${x}px`;
+            element.style.top = `${y}px`;
+
+            activePiece = element;
+        }
+    }
+
+    function movePiece(e: React.MouseEvent) {
+        const chessboard = chessboardRef.current;
+        if (activePiece && chessboard) {
+            const minX = chessboard.offsetLeft - 25;
+            const minY = chessboard.offsetTop  - 25;
+            const maxX = chessboard.offsetLeft - 75 + chessboard.clientHeight;
+            const maxY = chessboard.offsetTop  - 75 + chessboard.clientWidth;
+            const x = e.clientX - 50; //fix this when flex scaling
+            const y = e.clientY - 50;
+            activePiece.style.position = "absolute";
+            
+            if (x < minX) {
+                activePiece.style.left = `${minX}px`;
+            } else if (x > maxX) {
+                activePiece.style.left = `${maxX}px`;
+            } else {
+                activePiece.style.left = `${x}px`;
+            }
+
+            if (y < minY) {
+                activePiece.style.top = `${minY}px`;
+            } else if (y > maxY) {
+                activePiece.style.top = `${maxY}px`;
+            } else {
+                activePiece.style.top = `${y}px`;
+            }
+        }
+    }
+
+    function dropPiece(e: React.MouseEvent) {
+        if (activePiece) {
+            activePiece = null;
+        }
+    }
     let board = [];
     for (let j = yAxis.length - 1; j >= 0; j--) {
         for (let i = 0; i < xAxis.length; i++) {
@@ -84,6 +105,7 @@ export default function Chessboard() {
             onMouseDown={(e) => grabPiece(e)} 
             onMouseUp  ={(e) => dropPiece(e)}  
             id="chessboard"
+            ref={chessboardRef}
         >   
             {board}
         </div>
